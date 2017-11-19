f6bcc34b45b818d5080b7773abb3999a9e22acbe
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-19 18:38:21 +0200

    Create db-init.php

---
+++ db-init.php
@@ -0,0 +1,13 @@
+<?php
+//db-init.php
+try {
+        global $db;
+        $db = new PDO('mysql:host=localhost;dbname=cowrie;charset=utf8','PASSWORD HERE', 'USERNAME HERE');
+} catch (PDOEXCEPTION $e) {
+        echo "Connection failed" .$e->getMessage();
+}
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$status = $db->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+echo $status;
+?>
