26e20c0d3c3a35502cf3986b99a0ee0af82e95b4
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-20 20:34:17 +0200

    Update get-login-attempts.php

--- get-login-attempts.php
+++ get-login-attempts.php
@@ -6,7 +6,6 @@ function getLoginAttempts($localdb) {
         $attempts = array();
         $query = <<<SQL
         SELECT
-        `logindata`.`srcip`,
         `logindata`.`timestamp`
         FROM `logindata`
 SQL;
@@ -21,8 +20,36 @@ SQL;
 }
 
 $attempts = getLoginAttempts($db);
+//var_dump($attempts);
+
+$timestamparray = array();
+
+foreach ($attempts as $attempt) {
+        $stringArray = explode("T", $attempt['timestamp']);
+        $stringval = $stringArray[0];
+        $timestamparray[] = $stringval;
+}
+
+$vals = array_count_values($timestamparray);
+//print_r($vals);
 
 $attempt_array = array();
+
+if (!empty($vals)) {
+        foreach($vals as $key => $value) {
+                $attempt_array[] = array (
+                'date' => $key,
+                'value' => $value
+        );
+        }
+        $response = json_encode($attempt_array);
+} else {
+        echo "<h3>No data available! big error!</h3>";
+}
+
+echo $response;
+
+/*$attempt_array = array();
 
 if (!empty($attempts)) {
   foreach ($attempts as $attempt) {
@@ -30,7 +57,7 @@ if (!empty($attempts)) {
     $timestamp = $attempt['timestamp'];
     $attempt_array[] = array (
         'src_ip' => $srcip,
-        'timestamp' => $timestamp,
+        'date' => $timestamp,
     );
   }
   $response = json_encode($attempt_array);
@@ -38,6 +65,10 @@ if (!empty($attempts)) {
   echo '<span style="margin-left: 15px;">ree</span>';
 }
 
-header('Content-type: application/json');
-echo $response;
+
+// Toimii testattu:
+//header('Content-type: application/json');
+echo $response;*/
+
 ?>
+
