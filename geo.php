7cb18cd6861cd29f648842b608be76194fe363aa
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-21 21:18:55 +0200

    Update geo.php

--- geo.php
+++ geo.php
@@ -1,11 +1,49 @@
 <?php
-$ip_addr = "89.250.48.10";
-$geoplugin = unserialize( file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip_addr) );
 
-if ( is_numeric($geoplugin['geoplugin_latitude']) && is_numeric($geoplugin['geoplugin_longitude']) ) {
-
-$lat = $geoplugin['geoplugin_latitude'];
-$long = $geoplugin['geoplugin_longitude'];
+require_once('PATH_HERE/db-init.php');
+function getIpAddresses($localdb) {
+        $ip_addresses = array();
+        $query = "SELECT ip FROM sessions GROUP BY ip ORDER BY MAX(starttime) DESC LIMIT 35";
+        $get_addresses = $localdb->prepare($query);
+        $get_addresses->execute();
+        while ($row = $get_addresses->fetch(PDO::FETCH_ASSOC)) {
+                $ip_addresses[] = $row;
+        }
+        return $ip_addresses;
 }
-echo $ip_addr.';'.$lat.';'.$long;
+$ip_addresses = getIpAddresses($db);
+//var_dump($ip_addresses);
+$addresses = array();
+foreach ($ip_addresses as $address) {
+        //var_dump($address);
+        $stringval = $address['ip'];
+        $addresses[] = $stringval;
+}
+
+//var_dump($addresses);
+$addresseslen = count($addresses);
+
+$addresses_array = array();
+
+if (!empty($addresseslen)) {
+        for($i = 0; $i < $addresseslen; $i++){
+                $geoplugin = unserialize( file_get_contents('http://www.geoplugin.net/php.gp?ip='.$addresses[$i]) );
+                $addresses_array[] = array (
+                'scale'=>"0.5",
+                'title'=> $addresses[$i] ." ". $geoplugin['geoplugin_countryCode'] . "</br>" . $geoplugin['geoplugin_city'],
+                'latitude' => $geoplugin['geoplugin_latitude'],
+                'longitude' => $geoplugin['geoplugin_longitude'],
+                'type'=>"circle"
+        );
+        }
+
+        $response = json_encode($addresses_array);
+} else {
+        echo "<h3>No data available! big error!</h3>";
+}
+
+$geofile = fopen("geo.json", 'w')
+        or die("Unable to open file!");
+fwrite($geofile, $response);
+fclose($geofile);
 ?>
