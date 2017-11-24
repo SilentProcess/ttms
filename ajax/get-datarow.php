38ef97b51bccca915f451ad2f90102664f044695
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-24 13:54:48 +0200

    Create get-datarow.php

---
+++ ajax/get-datarow.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('/home/httpserver/db-init.php');
+
+function getDataRow($localdb) {
+        $datatable = array();
+        $query = "SELECT sessions.ip, sessions.starttime, auth.username, auth.password FROM sessions INNER JOIN auth ON sessions.id=auth.session GROUP BY sessions.id, auth.id ORDER BY MAX(sessions.starttime) DESC LIMIT 10;";
+        $datarow = $localdb->prepare($query);
+        $datarow->execute();
+
+        while ($row = $datarow->fetch(PDO::FETCH_ASSOC)) {
+                $datatable[] = $row;
+        }
+        return $datatable;
+}
+
+$datatable = getDataRow($db);
+//var_dump($datatable);
+
+$data_array = array();
+
+if(!empty($datatable)) {
+        foreach($datatable as $value) {
+                $data_array[] = array (
+                'ip' => $value['ip'],
+                'username' => $value['username'],
+                'password' => $value['password'],
+                'timestamp' => $value['starttime']
+        );
+        }
+        $response = json_encode($data_array);
+} else {
+        echo "<h3>No data available! huge error!</h3>";
+}
+echo $response;
+?>
