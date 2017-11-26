4dfb1e6e3b8af5f3f1c46df1ac96662f8d6269a1
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-26 18:47:18 +0200

    Update get-usernames.php

--- ajax/get-usernames.php
+++ ajax/get-usernames.php
@@ -1,7 +1,9 @@
 <?php
 
+// initialize database
 require_once('YOUR_PATH_HERE/db-init.php');
 
+// this function queries the top 10 attempted usernames and reads them into an array
 function getUserNames($localdb) {
         $usernames = array();
         $query = "SELECT username, COUNT(username) FROM auth GROUP BY username ORDER BY COUNT(username) DESC LIMIT 10";
@@ -19,6 +21,7 @@ $usernames = getUserNames($db);
 
 $userarray = array();
 
+// read the data from the array into another array and encode it to json
 if(!empty($usernames)) {
         foreach($usernames as $value) {
                 $userarray[] = array (
