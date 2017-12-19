6374d65e6c3fb4623d5650c561fac477086f1dc0
SilentProcess <SilentProcess@users.noreply.github.com>
2017-12-19 18:11:18 +0200

    Update list.php

--- inputdata/list.php
+++ inputdata/list.php
@@ -2,9 +2,17 @@
 // initialize database
 require_once('db-init.php');
 // read all login attempt timestamps into an array
-function createList($localdb) {
+$order = isset($_GET['a']) ? $_GET['a'] : '';
+if($order == "timestamp") {
+        $dataorder = "timestamp";
+} elseif ($order == "input") {
+        $dataorder = "count";
+} else {
+        $dataorder = "timestamp";
+}
+function createList($localdb, $dataorder) {
         $data = array();
-        $query = "SELECT DISTINCT input.session, count(input.input) AS count, auth.timestamp FROM input INNER JOIN auth ON input.session=auth.session GROUP BY input.session, auth.timestamp HAVING count>3 ORDER BY MAX(auth.timestamp) DESC";
+        $query = "SELECT DISTINCT input.session, count(input.input) AS count, convert_tz(auth.timestamp,'+00:00','+02:00') AS timestamp, sessions.ip FROM input INNER JOIN auth on input.session=auth.session INNER JOIN sessions on auth.session=sessions.id GROUP BY input.session, auth.timestamp HAVING count>3 ORDER BY {$dataorder} DESC;";
         $dataresult = $localdb->prepare($query);
         $dataresult->execute();
         while ($row = $dataresult->fetch(PDO::FETCH_ASSOC)) {
@@ -12,14 +20,14 @@ function createList($localdb) {
         }
         return $data;
 }
-$data = createList($db);
+$data = createList($db, $dataorder);
 //var_dump($data);
 // separate date-month-year and hour-minute-second parts and read the day-month-year parts
 // into an array
 
-function printData($datalist) {
+function printData($datalist, $order) {
         foreach ($datalist as $list) {
-                echo "<tr><td><a href='index.php?id={$list['session']}'>{$list['session']}</a></td>"  ."<td>{$list['count']}</td>" ."<td>{$list['timestamp']}</td></tr>";
+                echo "<tr><td><a href='index.php?id={$list['session']}&a={$order}'>{$list['session']}</a></td>"  ."<td>{$list['count']}</td>" ."<td>{$list['timestamp']}</td>" ."<td>{$list['ip']}</td></tr>";
         }
 }
 
@@ -30,8 +38,9 @@ $html_start = <<<HTMLEND
         <table>
         <tr>
                 <th>session</th>
-                <th>inputs</th>
-                <th>timestamp</th>
+                <th><a href="index.php?a=input">inputs</a></th>
+                <th><a href="index.php?a=timestamp">timestamp</a></th>
+                <th>ip</th>
         </tr>
 HTMLEND;
 
@@ -42,6 +51,6 @@ $html_end = <<<HTMLEND
 HTMLEND;
 
 echo $html_start;
-printData($data);
+printData($data, $order);
 echo $html_end;
 ?>
